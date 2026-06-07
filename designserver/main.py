f75bec7662c80226c751aa6e13106e18e9ddae18
Aman Bhatt <amanb2704@gmail.com>
2026-06-07 11:42:31 +0530

    backend change test

--- designserver/main.py
+++ designserver/main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from designserver!")
+from pycrdt import Map, Doc
+
+root = Map({'aman':{'name':'Aman','lastName':'Bhatt'}})
+doc = Doc()
+doc['root'] = root
 
 
-if __name__ == "__main__":
-    main()
+secondRoot = Map({'aman':{'name':'Aman','lastName':'Bhatt'}})
+secondDoc = Doc()
+secondDoc['root'] = secondRoot
+
+secondRoot['naman'] = {'name':'Naman','lastName':'Ttahb'}
+
+update1 = secondDoc.get_update()
+
+secondRoot.pop('naman')
+
+update2 = secondDoc.get_update()
+
+
+
+doc.apply_update(update2)
+
+print(doc.get('root',type=Map))
+
+doc.apply_update(update1)
+
+print(doc.get('root',type=Map))
+print(secondDoc.get('root',type=Map))
+
+
